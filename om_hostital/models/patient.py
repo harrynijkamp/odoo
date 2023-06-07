7b9fda7a90b11b81878bb6c2d54720827613958e
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-07 20:17:18 +0200

    Update patient.py

--- om_hostital/models/patient.py
+++ om_hostital/models/patient.py
@@ -2,5 +2,11 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
+    _description = "Patient Records"
 
-
+    name = fields.Char(string='Name', required=True)
+    age = fields.Integer(string='Age')
+    is_child = fields.Boolean(string='Is Child?')
+    notes = fields.Text(string='Notes')
+    gender = fields.Seleection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string='Gender')
+    
