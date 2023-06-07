484de59780ac5f82e901ebce3d345a3ed96e5926
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-07 19:25:52 +0200

    ...

--- school_management/__manifest__.py
+++ school_management/__manifest__.py
@@ -8,6 +8,7 @@
         'version': '16.0.1.0.0',
         #'post_init_hook': "create_student",
         'depends': ['base', 'contacts'],
+        
         'data': [
             # 'security/ir.model.access.csv',
             'views/school.xml',
