482e8e88a63844d139ce27c5bad100af2bbb45f1
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-07 20:18:20 +0200

    Update patient.py

--- om_hostital/models/patient.py
+++ om_hostital/models/patient.py
@@ -8,5 +8,5 @@ class HospitalPatient(models.Model):
     age = fields.Integer(string='Age')
     is_child = fields.Boolean(string='Is Child?')
     notes = fields.Text(string='Notes')
-    gender = fields.Seleection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string='Gender')
-    
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string='Gender')
+
