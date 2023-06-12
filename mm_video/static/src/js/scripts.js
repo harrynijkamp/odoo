c89a934f83602e321658f1a9eb4f9ef3024b4b54
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-12 16:03:22 +0200

    Update scripts.js

--- mm_video/static/src/js/scripts.js
+++ mm_video/static/src/js/scripts.js
@@ -1,25 +1,10 @@
 /** @omm_video **/
-odoo.define('metafoormedia.video.scripts', function(require) {
+odoo.define('mm_video.scripts', function(require) {
     'use strict';
     
     function generateVimeoPlayer() {
         console.log('Play the movie!!');
     };
 
-    //generateVimeoPlayer();
-
-    var url = window.location.href;
-    //var page_url = url.replace('#', '?');
-    //var params = parseURLParams(page_url);
-
-    //const pageHref = "?page=1&query=javascript";
-
-    // Construct a new object and pass the page href to URLSearchParams
-    //const searchParams = new URLSearchParams(pageHref.substring(pageHref.indexOf('?')));
-    
-    //console.log(searchParams);
-    // Output:
-    // URLSearchParams { 'page' => '1', 'query' => 'javascript' }
-
-    console.log(url);
+    generateVimeoPlayer();
 });
