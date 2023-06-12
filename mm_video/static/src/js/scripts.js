6dc1030e11705a334de42d1830135f2e324e9566
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-12 15:53:30 +0200

    Update scripts.js

--- mm_video/static/src/js/scripts.js
+++ mm_video/static/src/js/scripts.js
@@ -9,7 +9,17 @@ odoo.define('metafoormedia.video.scripts', function(require) {
     //generateVimeoPlayer();
 
     var url = window.location.href;
-    var page_url = url.replace('#', '?');
-    var params = parseURLParams(page_url);
-    console.log(params);
+    //var page_url = url.replace('#', '?');
+    //var params = parseURLParams(page_url);
+
+    //const pageHref = "?page=1&query=javascript";
+
+    // Construct a new object and pass the page href to URLSearchParams
+    //const searchParams = new URLSearchParams(pageHref.substring(pageHref.indexOf('?')));
+    
+    //console.log(searchParams);
+    // Output:
+    // URLSearchParams { 'page' => '1', 'query' => 'javascript' }
+
+    console.log(url);
 });
