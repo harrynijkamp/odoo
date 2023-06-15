1f7eae5168913d7d5e66cf19158d6d7b1f652ca1
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-15 13:16:38 +0200

    Update video.py

--- mm_video/models/video.py
+++ mm_video/models/video.py
@@ -14,6 +14,6 @@ class Video(models.Model):
     @api.depends('project_id')
     def _compute_partner(self):
         for record in self:
-            partner_id = self.env['project.project'].browser(record.project_id).partner_id
-            record.partner = self.env['res.partner'].browser(partner_id).name
+            partner_id = self.env['project.project'].browse(record.project_id).partner_id
+            record.partner = self.env['res.partner'].browse(partner_id).name
 
