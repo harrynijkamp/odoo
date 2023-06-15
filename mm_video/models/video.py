a5d76ecf8daac8097ec37d8654257a1e6284496e
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-15 19:50:27 +0200

    ...

--- mm_video/models/video.py
+++ mm_video/models/video.py
@@ -9,4 +9,4 @@ class Video(models.Model):
     name = fields.Char(string='Name', required=True, tracking=True)
     vimeo_link = fields.Char(string='Vimeo link', required=True, tracking=True) 
     project_id = fields.Many2one('project.project', string='Project', required=True, tracking=True)
-    partner_id = fields.many2many('res.partner', string='Involvement', tracking=True)
+    partner_id = fields.Many2many('res.partner', string='Involvement', tracking=True)
