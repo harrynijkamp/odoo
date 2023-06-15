d64c75d1929a11c116b4f6d2f55878c149d93479
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-15 12:12:32 +0200

    Update video.py

--- mm_video/models/video.py
+++ mm_video/models/video.py
@@ -9,4 +9,9 @@ class Video(models.Model):
     name = fields.Char(string='Name', required=True, tracking=True)
     vimeo_link = fields.Char(string='Vimeo link', required=True, tracking=True) 
     project_id = fields.Many2one('project.project', string='Project', required=True, tracking=True)
+    partner_count = fields.Integer(string='Partner', compute='_compute_partner_id')
+
+    def _compute_partner_id(self):
+        partner_count = self.env['project.project'].search_count([('partner_id', '=', self.id)])
+        self.partner_count = partner_count
 
