d345fbcd6dfbd66fc18c4f97e7dcd7eb9de3f0ac
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-19 15:42:30 +0200

    Delete mm_video directory

--- mm_video/models/project.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class Project (models.Model):
-    _inherit = "project.project"
-
-    video_line_ids = fields.One2many('metafoormedia.video', 'project_id', string="Videos")
