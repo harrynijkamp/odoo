17c4e39dcd46d8c1d460249d7e92eda642982933
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-22 19:03:51 +0200

    Update res_partner.py

--- mm_customizations/models/res_partner.py
+++ mm_customizations/models/res_partner.py
@@ -2,10 +2,9 @@ from odoo import models, fields
 
 class ResPartner (models.Model):
     _inherit = "res.partner"
-    _inherit = ["mail.thread"]
 
     my_field = fields.Selection([
         ('value1', 'Label 1'),
         ('value2', 'Label 2'),
         ('value3', 'Label 3')
-    ], string='My Field', tracking=True)
+    ], string='My Field')
