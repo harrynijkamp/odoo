b1e965ee0e59a3545ab9f003934cce1002c21ea2
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-22 17:27:58 +0200

    Update partner.py

--- mm_video/models/partner.py
+++ mm_video/models/partner.py
@@ -4,7 +4,7 @@ class Partner (models.Model):
     _inherit = "res.partner"
 
     video_ids = fields.Many2many('metafoormedia.video', 'video_partner_rel', 'video_id', 'partner_id', string='Video Involvement')
-    gender = fields.Selection([
+    gen = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female')
     ], string='Gender')
