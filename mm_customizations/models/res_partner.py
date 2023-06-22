71bdcd445bfbb2128ad2dbb6e199fb783bfeee28
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-22 19:06:49 +0200

    ....

--- mm_customizations/models/res_partner.py
+++ mm_customizations/models/res_partner.py
@@ -3,8 +3,4 @@ from odoo import models, fields
 class ResPartner (models.Model):
     _inherit = "res.partner"
 
-    gender = fields.Selection([
-        ('male', 'Male'),
-        ('female', 'Female')
-        
-    ], string='Gender')
+    
