ce3e911dbfca5e1017f940abe0ac7df397dbb91c
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-22 16:39:37 +0200

    new app

---
+++ mm_customizations/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Metafoor Media Video',
+    'version': '0.0.1',
+    'summary': 'Metafoor Media Customizations',
+    'sequence': 4,
+    'description': 'Several customizations on Odoo to improve the Business Case off Metafoor Media.',
+
+    'author': 'Harry Nijkamp',
+    'company': 'Metafoor Media BV',
+    'website': 'www.metafoormedia.nl',
+    'category': 'Customization',
+    'depends': ['base', 'mail'],
+    
+    'data': [
+        
+    ],
+    
+    'application': True,
+}
