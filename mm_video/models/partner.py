d9e1d07e0cd45e3bdca7ecadba54d550ae95a5a2
harrynijkamp <135700118+harrynijkamp@users.noreply.github.com>
2023-06-22 18:45:33 +0200

    test

--- mm_video/models/partner.py
+++ mm_video/models/partner.py
@@ -4,3 +4,9 @@ class Partner (models.Model):
     _inherit = "res.partner"
 
     video_ids = fields.Many2many('metafoormedia.video', 'video_partner_rel', 'video_id', 'partner_id', string='Video Involvement')
+    my_field = fields.Selection([
+        ('value1', 'Label 1'),
+        ('value2', 'Label 2'),
+        ('value3', 'Label 3')
+    ], string='My Field')
+    
